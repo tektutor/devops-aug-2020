dfd8347b194211e01ddf1def8278e4c9ed4cd66b
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-08-19 23:36:17 -0700

    Using properties files to add math operator support.

--- Day3/RPNCalculator/src/main/java/org/tektutor/MathFactory.java
+++ Day3/RPNCalculator/src/main/java/org/tektutor/MathFactory.java
@@ -1,23 +1,47 @@
 package org.tektutor;
 
+import java.util.Map;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.Properties;
+import java.util.Enumeration;
+import java.io.InputStream;
 
 public class MathFactory {
-	private static	HashMap<String, String> classNameMap;
+	private static	Map<String, String> classNameMap;
 
 	static {
+           try{
 		classNameMap = new HashMap<String,String>();
 
-		classNameMap.put ( "+", "org.tektutor.Addition" );
-		classNameMap.put ( "-", "org.tektutor.Subtraction" );
-		classNameMap.put ( "*", "org.tektutor.Multiplication" );
-		classNameMap.put ( "/", "org.tektutor.Division" );
+		InputStream fileReader 
+			= MathFactory.class
+				.getClassLoader()
+				.getResourceAsStream("rpn.properties");
+
+		Properties properties = new Properties();
+		properties.load ( fileReader );
+		
+		Set<Object> set = properties.keySet();
+		String key, value;
+		Enumeration entry = properties.propertyNames();
+	
+		while ( entry.hasMoreElements() ) {
+
+			key   = (String) entry.nextElement();
+			value = (String) properties.getProperty( key );
+
+			classNameMap.put ( key, value );		 
+		}
+           }
+           catch ( Exception e ) {
+		e.printStackTrace();
+	   }
 	}
 
 	public static IMathOperator getObject ( String operator ) {
 
 		IMathOperator mathOperator = null;
-
 		String className = classNameMap.get ( operator );
 
 		try {
