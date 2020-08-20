7482fc8520c1942793c10730c39c4b70dbfb4203
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-08-20 23:49:17 +0000

    Added CSharp project.

---
+++ Day5/CSharpProject/HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TekTutor
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
