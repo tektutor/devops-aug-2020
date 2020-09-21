97f34a8ea444dbd17c109063e97291aa1a7f0b94
Jeganathan Swaminathan <mail2jegan@gmail.com>
2020-09-21 03:21:20 -0700

    Introducted bug to demo CI.

--- Day3/RPNCalculator/src/main/java/org/tektutor/Addition.java
+++ Day3/RPNCalculator/src/main/java/org/tektutor/Addition.java
@@ -3,7 +3,7 @@ package org.tektutor;
 public class Addition implements IMathOperator {
 
 	public double evaluate ( double firstNumber, double secondNumber ) {
-		return firstNumber - secondNumber;
+		return firstNumber + secondNumber;
 	}
 
 }
